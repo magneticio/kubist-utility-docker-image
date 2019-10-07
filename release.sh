aff60d7b448489fcee555a5241bc4c81ddcfa0d0
berkgokden <berkgokden@gmail.com>
2019-10-07 14:47:53 +0200

    No cache added

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ set -e
 
 VERSION=$1
 
-docker build -t magneticio/kubist-util:$VERSION .
+docker build  --no-cache -t magneticio/kubist-util:$VERSION .
 docker push magneticio/kubist-util:$VERSION
 
 docker tag magneticio/kubist-util:$VERSION magneticio/kubist-util:Latest
